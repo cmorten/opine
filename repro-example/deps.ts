0664d33414dd3e765d6e486c60cae3b7531c562b
Craig Morten <46491566+asos-craigmorten@users.noreply.github.com>
2020-06-26 17:39:25 +0100

    [#29] Automate typedoc publish (#30)
    * feat: automate typedoc updates
    * chore: fix branch name in PR template

---
+++ repro-example/deps.ts
@@ -0,0 +1,3 @@
+export { serve } from "https://deno.land/std@0.58.0/http/server.ts";
+export { Status } from "https://deno.land/std@0.56.0/http/http_status.ts";
+export { encodeUrl } from "https://deno.land/x/encodeurl@1.0.0/mod.ts";
