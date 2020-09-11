ac289ded16c5d28724e98520b663bfbf5bc68799
Ben Gubler <nebrelbug@gmail.com>
2020-09-11 15:32:58 -0600

    [#61] Add an example of using the Eta template engine (#65)
    * Add an example of the Eta template engine

---
+++ examples/eta/README.md
@@ -0,0 +1,13 @@
+# eta
+
+Example of how to use Opine's template engine and rendering capabilities with the `eta` template engine.
+
+View Eta's documentation at https://eta.js.org
+
+## How to run this example
+
+Run this example using:
+
+```bash
+deno run --allow-net --allow-read --unstable ./examples/eta/index.ts
+```
---
+++ examples/eta/index.ts
@@ -0,0 +1,50 @@
+/**
+ * Run this example using:
+ * 
+ *    deno run --allow-net --allow-read --unstable ./examples/eta/index.ts
+ * 
+ *    Note: you have to use --unstable because Eta uses the std 'fs' module (which requires --unstable)
+ */
+
+import { opine, serveStatic } from "../../mod.ts";
+import { renderFile } from "https://deno.land/x/eta@v1.6.0/mod.ts";
+import { join, dirname } from "../../deps.ts";
+
+const app = opine();
+const __dirname = dirname(import.meta.url);
+
+// Register Eta as .html.
+app.engine(".html", renderFile);
+
+// Optional since opine defaults to CWD/views
+app.set("views", join(__dirname, "views"));
+
+// Path to our public directory
+app.use(serveStatic(join(__dirname, "public")));
+
+// Without this you would need to
+// supply the extension to res.render()
+// ex: res.render('users.html').
+app.set("view engine", "html");
+
+// Disable caching (comment out in production)
+app.set("view cache", false);
+
+// Dummy users
+const users = [
+  { name: "Deno", email: "deno@denoland.com" },
+  { name: "SuperDeno", email: "superdeno@denoland.com" },
+  { name: "Ada Lovelace", email: "ada@denoland.com" },
+  { name: "Deno the Dinosaur", email: "denosaur@denoland.com" },
+];
+
+app.get("/", (req, res) => {
+  res.set("cache-control", "no-store").render("index", {
+    users,
+    title: "Eta example",
+    header: "Some users",
+  });
+});
+
+app.listen(3000);
+console.log("Opine started on port 3000");
