916a228e40073fa19911e8d4e3d842791bd1478f
v1rtl <pilll.pl22@gmail.com>
2021-04-09 20:57:39 +0300

    Add GraphQL example

---
+++ examples/graphql/index.ts
@@ -0,0 +1,39 @@
+/**
+ * Run this example using:
+ *
+ *    deno run --allow-net --allow-read ./examples/graphql/index.ts
+ *
+ *    if have the repo cloned locally OR
+ *
+ *    deno run --allow-net --allow-read https://raw.githubusercontent.com/asos-craigmorten/opine/main/examples/graphql/index.ts
+ *
+ *    if you don't!
+ *
+ */
+
+import { opine } from "../../mod.ts";
+import { GraphQLHTTP } from "https://deno.land/x/gql@0.1.1/mod.ts";
+import { makeExecutableSchema } from "https://deno.land/x/graphql_tools@0.0.0/mod.ts";
+import { gql } from "https://deno.land/x/graphql_tag@0.0.0/mod.ts";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => `Hello World!`,
+  },
+};
+
+const schema = makeExecutableSchema({ resolvers, typeDefs });
+
+const app = opine();
+
+app.use("/graphql", GraphQLHTTP({ schema, graphiql: true }));
+
+app.listen(3000);
+
+console.log("Opine started on port 3000");
