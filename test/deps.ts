3fc82029632db48767007918332a6588dbdd28bd
cmorten <craig.morten@hotmail.co.uk>
2021-06-25 12:40:23 +0100

    test: upgrade superdeno version

--- test/deps.ts
+++ test/deps.ts
@@ -1,8 +1,8 @@
 export { deferred } from "https://deno.land/std@0.99.0/async/deferred.ts";
 export type { Deferred } from "https://deno.land/std@0.99.0/async/deferred.ts";
 export { expect, mock } from "https://deno.land/x/expect@v0.2.9/mod.ts";
-export { superdeno } from "https://deno.land/x/superdeno@4.2.1/mod.ts";
+export { superdeno } from "https://deno.land/x/superdeno@4.3.0/mod.ts";
 export type {
   IRequest as SuperDenoRequest,
   IResponse as SuperDenoResponse,
-} from "https://deno.land/x/superdeno@4.2.1/mod.ts";
+} from "https://deno.land/x/superdeno@4.3.0/mod.ts";
--- version.ts
+++ version.ts
@@ -6,4 +6,4 @@ export const VERSION = "1.5.3";
 /**
  * Supported version of Deno.
  */
-export const DENO_SUPPORTED_VERSIONS: string[] = ["1.11.1"];
+export const DENO_SUPPORTED_VERSIONS: string[] = ["1.11.2"];
