8bcf2d48a2d9671c56adf0526e6d38589142cadc
AEtheve <30652308+AEtheve@users.noreply.github.com>
2022-03-20 19:44:42 +0100

    Update HTTPSOptions

--- src/types.ts
+++ src/types.ts
@@ -19,7 +19,10 @@ declare global {
 }
 
 export type HTTPOptions = Omit<Deno.ListenOptions, "transport">;
-export type HTTPSOptions = Omit<Deno.ListenTlsOptions & {"certFile":string; "keyFile":string}, "transport">;
+export type HTTPSOptions = Omit<
+  Deno.ListenTlsOptions & { "certFile": string; "keyFile": string },
+  "transport"
+>;
 
 export type DenoResponseBody = undefined | string | Uint8Array | Deno.Reader;
 export type ResponseBody =
